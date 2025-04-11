097a8d08c680df26feaee3c85d905d777485831a
SDJ2023 <129523067+SDJ2023@users.noreply.github.com>
2025-04-11 04:08:16 +0000

    Upload cancel file

--- main.py
+++ main.py
@@ -25,9 +25,20 @@ def index():
     # Pass the DataFrame data to the HTML template
     return render_template('index.html', data=df_dict, myid=myid)
 
+
 @app.route('/perform', methods=['GET', 'POST'])
 def perform():
+    # Create a simple Pandas DataFrame
+    # data = pd.read_csv("./static/Performance_NOVEMBER 2024.csv")
+    # df = pd.DataFrame(data)
+    #df = df.loc[df['RefNo']] == myid
+    # Convert DataFrame to a dictionary to pass to the template
+    #df_dict = df.to_dict()
+    # print(df)
+    # Pass the DataFrame data to the HTML template
+    # return render_template('index.html', data=df_dict, myid=myid)
     return render_template('perform.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
